f5a3dfda5cf649247d33969bcbb8a9e4a7709855
zhangwensen <zhangwensen@253.com>
2025-07-24 16:28:06 +0800

    feat(util): 添加 SpringContextUtil 和 UserIdInjectorUtil 工具类- SpringContextUtil：用于获取 Spring 容器中的 Bean - UserIdInjectorUtil：用于自动从上下文获取 userId 并设置到 DTO 对象中

---
+++ src/main/java/com/acco/life/util/SpringContextUtil.java
@@ -0,0 +1,43 @@
+package com.acco.life.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring上下文工具类，用于获取Spring容器中的Bean
+ */
+@Component
+public class SpringContextUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextUtil.applicationContext = applicationContext;
+    }
+
+    /**
+     * 获取Spring容器中的Bean
+     *
+     * @param clazz Bean的类
+     * @param <T>   Bean的类型
+     * @return Bean实例
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+
+    /**
+     * 根据名称和类型获取Spring容器中的Bean
+     *
+     * @param name  Bean的名称
+     * @param clazz Bean的类
+     * @param <T>   Bean的类型
+     * @return Bean实例
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return applicationContext.getBean(name, clazz);
+    }
+}
---
+++ src/main/java/com/acco/life/util/UserIdInjectorUtil.java
@@ -0,0 +1,74 @@
+package com.acco.life.util;
+
+import reactor.core.publisher.Mono;
+import java.lang.reflect.Method;
+
+/**
+ * UserId注入工具类
+ * 用于自动从上下文获取userId并设置到DTO对象中
+ */
+public class UserIdInjectorUtil {
+    
+    /**
+     * 自动通过反射设置DTO对象的userId字段
+     *
+     * @param dtoMono DTO对象的Mono流
+     * @param <T> DTO类型
+     * @return 设置userId后的DTO对象的Mono流
+     */
+    public static <T> Mono<T> withUserId(Mono<T> dtoMono) {
+        return UserUtil.getCurrentUser()
+                .flatMap(userDto -> {
+                    Integer userId = userDto.getId();
+                    return dtoMono.map(dto -> {
+                        try {
+                            // 通过反射设置userId
+                            Method setUserIdMethod = dto.getClass().getMethod("setUserId", Integer.class);
+                            setUserIdMethod.invoke(dto, userId);
+                        } catch (Exception e) {
+                            // 如果没有setUserId方法或设置失败，则忽略
+                        }
+                        return dto;
+                    });
+                });
+    }
+
+    /**
+     * 自动通过反射设置DTO对象的userId字段并保存
+     *
+     * @param dtoMono DTO对象的Mono流
+     * @param service 服务对象，用于保存DTO
+     * @param <T> DTO类型
+     * @param <R> 返回结果类型
+     * @return 保存结果的Mono流
+     */
+    public static <T, R> Mono<R> withUserIdAndSave(Mono<T> dtoMono, Object service) {
+        return withUserId(dtoMono)
+                .flatMap(dto -> {
+                    try {
+                        // 通过反射调用服务的save方法
+                        Method saveMethod = service.getClass().getMethod("save", Mono.class);
+                        return (Mono<R>) saveMethod.invoke(service, Mono.just(dto));
+                    } catch (Exception e) {
+                        throw new RuntimeException("Failed to save entity", e);
+                    }
+                });
+    }
+    
+    /**
+     * 自动注入userId到DTO对象并执行操作
+     *
+     * @param dtoMono DTO对象的Mono流
+     * @param function 处理函数，接收DTO对象和userId
+     * @param <T> DTO类型
+     * @param <R> 返回结果类型
+     * @return 处理结果的Mono流
+     */
+    public static <T, R> Mono<R> withUserId(Mono<T> dtoMono, java.util.function.BiFunction<T, Integer, R> function) {
+        return UserUtil.getCurrentUser()
+                .flatMap(userDto -> {
+                    Integer userId = userDto.getId();
+                    return dtoMono.map(dto -> function.apply(dto, userId));
+                });
+    }
+}
